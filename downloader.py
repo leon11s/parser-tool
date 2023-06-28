3b9abc710225b21d6fa88ea0cd74c63238522590
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2023-06-28 12:00:15 +0200

    fix folder validation

--- downloader.py
+++ downloader.py
@@ -1,11 +1,12 @@
+import os
+
 import requests
 
-from file_helpers import validate_folder_path
 
-
-def download(url: str, file_path: str) -> None:
+def download(url: str, folder_path: str) -> None:
     """Download file from url and save it locally under `file_path`"""
-    validate_folder_path(file_path)
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, url.split("/")[-1])
     with open(file_path, "wb") as file:
         response = requests.get(url)
         file.write(response.content)
@@ -13,9 +14,9 @@ def download(url: str, file_path: str) -> None:
 
 
 def main():
-    url = "https://raw.githubusercontent.com/Opensourcefordatascience/Data-sets/master/blood_pressure.csv"
-    file_path = "data/blood_pressure.csv"
-    download(url, file_path)
+    url = "https://podatki.gov.si/dataset/83c7e79c-eff6-4e99-8de3-c1942ba86188/resource/84f76597-64d8-4e9e-9351-4df81b08aae5/download/lokacijezbiralnikovzaodpadnojedilnooljevobinisevnica.csv"
+    folder = "data"
+    download(url, folder)
 
 
 main()
