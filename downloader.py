31c814a7578387d7ffada77a569fdad1fa89884c
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2023-06-28 11:44:48 +0200

    add downloader

--- downloader.py
+++ downloader.py
@@ -1,1 +1,21 @@
-url = "https://raw.githubusercontent.com/Opensourcefordatascience/Data-sets/master/blood_pressure.csv"
+import requests
+
+from file_helpers import validate_folder_path
+
+
+def download(url: str, file_path: str) -> None:
+    """Download file from url and save it locally under `file_path`"""
+    validate_folder_path(file_path)
+    with open(file_path, "wb") as file:
+        response = requests.get(url)
+        file.write(response.content)
+        print(f"Downloaded {url} to {file_path}.")
+
+
+def main():
+    url = "https://raw.githubusercontent.com/Opensourcefordatascience/Data-sets/master/blood_pressure.csv"
+    file_path = "data/blood_pressure.csv"
+    download(url, file_path)
+
+
+main()
