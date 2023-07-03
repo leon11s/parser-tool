258f90d63d62d663fb7c126f38b28ee0d4135d72
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2023-07-03 14:00:32 +0200

    add image extraction

---
+++ extract_image_text/extract.py
@@ -0,0 +1,5 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+print(pytesseract.image_to_string(Image.open("test.jpg")))
