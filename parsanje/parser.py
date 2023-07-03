9329a95df65950cb6260fcfeec1e6131302a4ebd
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2023-07-03 09:19:47 +0200

    add convert_webpage_to_pdf

--- parsanje/parser.py
+++ parsanje/parser.py
@@ -1,2 +1,24 @@
+from typing import Union
+
+import pdfkit
+
+
+def convert_webpage_to_pdf(
+    url: str,
+    output_file_path: str,
+    wkhtmltopdf_path: Union[str, None] = None,
+) -> None:
+    try:
+        pdfkit.from_url(
+            url,
+            output_file_path,
+            verbose=True,
+            configuration=pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path),
+        )
+    except OSError:
+        print("Error: wkhtmltopdf not found in the specified location.")
+
+
 # https://gd.eppo.int/search?k=ralstonia
 # https://gd.eppo.int/taxon/RALSSL
+# C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
